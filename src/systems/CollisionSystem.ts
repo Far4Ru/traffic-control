06a658a4e42b1dccd7e1a7432b00e985932c7c6e
Far4Ru <faty422@mail.ru>
2026-04-09 14:02:07 +0300

    Changed collision

--- src/systems/CollisionSystem.ts
+++ src/systems/CollisionSystem.ts
@@ -6,7 +6,7 @@ export class CollisionSystem extends System {
     super(60);
   }
 
-  update(world: World, _deltaTime: number): void {
+  update(world: World, deltaTime: number): void {
     const vehicles = world.getEntitiesWithComponent('vehicle');
 
     // Сброс состояния коллизий
@@ -14,7 +14,6 @@ export class CollisionSystem extends System {
       const collision = vehicle.getComponent<CollisionComponent>('collision');
       if (collision) {
         collision.colliding = false;
-        collision.stopped = false;
         collision.collisionWith = undefined;
       }
     }
@@ -22,12 +21,12 @@ export class CollisionSystem extends System {
     // Проверка коллизий
     for (let i = 0; i < vehicles.length; i++) {
       for (let j = i + 1; j < vehicles.length; j++) {
-        this.checkCollision(vehicles[i], vehicles[j]);
+        this.checkCollision(vehicles[i], vehicles[j], deltaTime);
       }
     }
   }
 
-  private checkCollision(v1: Entity, v2: Entity): void {
+  private checkCollision(v1: Entity, v2: Entity, deltaTime: number): void {
     const t1 = v1.getComponent<TransformComponent>('transform');
     const t2 = v2.getComponent<TransformComponent>('transform');
     const c1 = v1.getComponent<CollisionComponent>('collision');
@@ -42,17 +41,26 @@ export class CollisionSystem extends System {
       Math.pow(t2.position.y - t1.position.y, 2)
     );
 
-    if (distance < c1.radius + c2.radius) {
+    const minDistance = c1.radius + c2.radius;
+    
+    if (distance < minDistance) {
+      // Отталкиваем машины друг от друга
+      const angle = Math.atan2(t2.position.y - t1.position.y, t2.position.x - t1.position.x);
+      const overlap = minDistance - distance;
+      const pushX = Math.cos(angle) * overlap * 0.5;
+      const pushY = Math.sin(angle) * overlap * 0.5;
+      
+      t1.position.x -= pushX;
+      t1.position.y -= pushY;
+      t2.position.x += pushX;
+      t2.position.y += pushY;
+      
+      // Снижаем скорость при столкновении
+      veh1.speed = Math.max(0, veh1.speed - 0.5 * deltaTime);
+      veh2.speed = Math.max(0, veh2.speed - 0.5 * deltaTime);
+      
       c1.colliding = true;
-      c1.stopped = true;
-      c1.collisionWith = v2.id;
-
       c2.colliding = true;
-      c2.stopped = true;
-      c2.collisionWith = v1.id;
-
-      veh1.speed = 0;
-      veh2.speed = 0;
     }
   }
 
@@ -61,9 +69,12 @@ export class CollisionSystem extends System {
 
     for (const vehicle of vehicles) {
       const collision = vehicle.getComponent<CollisionComponent>('collision');
+      const vehicleComp = vehicle.getComponent<VehicleComponent>('vehicle');
       if (collision) {
         collision.colliding = false;
-        collision.stopped = false;
+        if (vehicleComp && collision.collisionWith) {
+          vehicleComp.speed = Math.max(0, vehicleComp.speed * 0.5);
+        }
         collision.collisionWith = undefined;
       }
     }
